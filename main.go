2e69b22eb04c80bf22094532ceeda1680e630521
zsc <755717181@qq.com>
2018-12-02 15:36:35 +0800

    新建websocket

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"./queue"
+	"./websocket"
 	"fmt"
+	"flag"
 )
 
-func main() {
-	q := queue.Queue{1}
 
+
+func main() {
+
+	//新建队列
+	q := queue.Queue{1}
 	q.Push(1)
 	q.Push(3)
 	//fmt.Println(q.Pop())
@@ -16,5 +21,14 @@ func main() {
 	//fmt.Println(q.Pop())
 	//fmt.Println(q.IsEmpty())
 	fmt.Println(q.FindThingsAlreadyInside(1))
+
+	//新建websocket服务端
+	addr  := flag.String("a", "127.0.0.1:1234", "websocket server listen address")
+	flag.Parse()
+	wsServer := &websocket.WSServer{
+		ListenAddr : *addr,
+	}
+	//开始监听
+	wsServer.Start()
 
 	}
---
+++ speaker/speak.go
@@ -0,0 +1,45 @@
+package speaker
+
+
+
+import (
+"fmt"
+
+"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
+"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+aai "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/aai/v20180522"
+)
+
+func Speak() {
+
+	credential := common.NewCredential(
+		"AKIDwb5OlJEsKcjfSLqkIhvBMPIWZUPEDSzL",
+		"LXhgV9d4L3b8UlYhfdR4wgLXadEf0Uo7",
+	)
+	//type params struct {
+	//	Text string,
+	//	SessionId string,
+	//
+	//}
+	cpf := profile.NewClientProfile()
+	cpf.HttpProfile.Endpoint = "aai.ap-beijing.tencentcloudapi.com"
+	client, _ := aai.NewClient(credential, "ap-beijing", cpf)
+
+	request := aai.NewTextToVoiceRequest()
+
+	params := `{"Text":"欢迎   王明超同学","SessionId":"123","ModelType":1,"VoiceType":0}`
+	err := request.FromJsonString(params)
+	if err != nil {
+		panic(err)
+	}
+	response, err := client.TextToVoice(request)
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", response.ToJsonString())
+}
